8fcba389550b5d1bb52536bfc7b26cc98a73925f
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2021-10-26 21:49:01 +0200

    Contact form php

---
+++ portfolio/contact-handler.php
@@ -0,0 +1,21 @@
+<?php 
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+
+    $email_from = 'thomaswwpedersen@gmail.com';
+    $email_subject = "New Form Submission";
+    $email_body = "Fra: $firstname.\n". 
+                  "Efternavn: $lastname.\n".
+                  "Email: $email.\n".
+                  "Besked: $subject.\n";
+
+                  $to = "thomaswpedersen@hotmail.com";
+                  $headers = "From: $email_from \r\n";
+                  $headers .= "Reply-To: $visitor_email \r\n";
+                  mail($to,$email_subject,$email_body,$headers);
+
+                  header("Location: index.html");
+
+    ?>
